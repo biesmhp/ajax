20f342c242d1ec386d54c6280c836892117616f3
Brendan <brendan.rodriguezfranco@iesmiguelherrero.com>
2022-06-02 17:14:54 +0200

    recuperacion_ajax_ej5_v2

--- recuperacion/practicasAJAX/ej5/ej5.js
+++ recuperacion/practicasAJAX/ej5/ej5.js
@@ -1,39 +1,47 @@
 addEventListener('load',inicio,false)
 
 function inicio() {
+  // console.log(document.querySelector("#prueba"));
   // Access json to fill the initial select
-  var xjson = new XMLHttpRequest();
+  let xjson = new XMLHttpRequest()
 
-  xjson.open("GET", "paises y ciudades.json", true);
-  xjson.send(null);
+  xjson.open("GET", "paises y ciudades.json", true)
+  xjson.send(null)
 
   xjson.onreadystatechange = function() {
-    if (xjson.readyState == 4) {
-      if (xjson.status == 200) {
-        let myObj = JSON.parse(xjson.responseText);
-        addSelect(myObj.listadoPaises.pais,"#selPaises")
-      }
+    if (xjson.readyState == 4 && xjson.status == 200) {
+      let myObj = JSON.parse(xjson.responseText)
+      addSelect(myObj.listadoPaises.pais,"#selPaises")
+      // Show the cities
+      evento = document.querySelector("#btnEnviar")
+      evento.addEventListener('click',function (){
+        let pais = document.querySelector("#selPaises").value
+        // console.log(myObj.listadoPaises.pais[pais])
+        // Create info details of the country selected
+        addInfo(myObj.listadoPaises.pais[pais].capital,document.querySelector("#visualizado"))
+        addInfo(myObj.listadoPaises.pais[pais].textoCapital,document.querySelector("#visualizado"))
+      },false)
     }
-  };
-
-  // ##############WIP
-
-  evento = document.querySelector("#btnEnviar")
-  evento.addEventListener('click',function (){
-    // Create the select of countries
-
-  },false)
+  }
 }
 
 function addSelect(arr,objetivo) {
   let select = document.querySelector(objetivo)
   for (let i = 0; i < arr.length; i++) {
     let opcion = document.createElement("option")
-    // console.log(arr[i]);
     opcion.setAttribute("value",i)
     opcion.setAttribute("label",arr[i].nombre)
+    opcion.setAttribute("class","paises")
     select.appendChild(opcion)
   }
+  return select ? true : false
+}
+
+function addInfo(info,objetivo) {
+  // let infoNode = document.querySelector(objetivo)
+  let opcion = document.createElement("span")
+  opcion.textContent = info
+  objetivo.appendChild(opcion)
 }
 
 function muestraContenido() {
