0e420b4dd52d4baeac8769bb35b209a0ab538fde
Brendan <brendan.rodriguezfranco@iesmiguelherrero.com>
2022-06-13 18:52:46 +0200

    recuperacion_ajax_object_v2

--- recuperacion/ajax_json_object/js.js
+++ recuperacion/ajax_json_object/js.js
@@ -1,10 +1,49 @@
 addEventListener('load',inicio,false)
+let arrCategoria = ['Hogar','Calzado','Ropa','Accesorios']
 
 function inicio() {
-  evento = document.querySelector("#btnEnviar")
-  evento.addEventListener('click',function (e) {
+  //
+  let cajaDatosNode = document.querySelector(".inpDatos")
+  addSelect(arrCategoria,cajaDatosNode,"selCategoria")
+  addInput("text",cajaDatosNode,"btnEnviar")
+  // evento = document.querySelector("#btnEnviar")
+  // evento.addEventListener('click',function (e) {
+  //
+  // },false)
+}
 
-  },false)
+function addSelect(arr,objetivo,id) {
+  let select = document.createElement("select")
+  arguments.length == 3 ? select.setAttribute("id",id) : null
+  for (let i = 0; i < arr.length; i++) {
+    let opcion = document.createElement("option")
+    opcion.setAttribute("value",i)
+    opcion.setAttribute("label",`${arr[i]}`)
+    opcion.setAttribute("class","opcionSelect")
+    select.appendChild(opcion)
+  }
+  objetivo.appendChild(select)
+  return objetivo ? true : false
+}
+
+function addInput(tipo,objetivo,id) {
+  let input = document.createElement("input")
+  arguments.length == 3 ? input.setAttribute("id",id) : null
+  input.setAttribute("type",tipo)
+  input.setAttribute("class","input")
+  switch (true) {
+    case tipo=="number":
+      input.setAttribute("min",0)
+      input.setAttribute("value",0)
+    break;
+    case tipo=="button":
+      input.setAttribute("value","boton")
+    break;
+    default:
+      input.setAttribute("placeholder","Introduce texto")
+  }
+  objetivo.appendChild(input)
+  return objetivo ? true : false
 }
 
 class Articulo {
