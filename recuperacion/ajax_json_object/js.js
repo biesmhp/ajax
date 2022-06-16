6d80fa877bf1c7fbf76ee5b67aba0e2904a4223b
biesmhp <brendan.rodriguezfranco@iesmiguelherrero>
2022-06-16 11:35:47 +0200

    recuperacion_ajax_object_v2.25

--- recuperacion/ajax_json_object/js.js
+++ recuperacion/ajax_json_object/js.js
@@ -38,6 +38,10 @@ function inicio() {
       // console.log(new Date(fecha1Node.value).toLocaleDateString("es-ES"));
       let fecha2Node = document.querySelector("#inpFecha2")
       fecha2Node.value = fecha1Node.value
+      let fecha3Node = document.querySelector("#inpFecha3")
+      let fecha4Node = document.querySelector("#inpFecha4")
+      let visual = document.querySelector("#visualizado")
+      document.querySelector("#visualizado").innerHTML = calendar(new Date(fecha1Node.value))
     },false)
   },false)
 }
@@ -155,4 +159,53 @@ class Articulo {
     return `Categoría: ${this.categoria}, codigo: ${this.codigo}, precio con IVA: ${this.puv}, nº ventas: ${this.ventas}.\n
     Descripcion: ${this.descripcion}`
   }
+}
+
+function calendar(date) {
+  // If no parameter is passed use the current date.
+  if(date == null)
+     date = new Date();
+
+  day = date.getDate();
+  month = date.getMonth();
+  year = date.getFullYear();
+
+  months = new Array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+
+  this_month = new Date(year, month, 1);
+  next_month = new Date(year, month + 1, 1);
+
+  // Find out when this month starts and ends.
+  first_week_day = this_month.getUTCDay();
+  days_in_this_month = Math.round((next_month.getTime() - this_month.getTime()) / (1000 * 60 * 60 * 24));
+
+  calendar_html = `<table class="calendario">`;
+  calendar_html += `<tr><td colspan="7">` + months[month] + ` ` + year + `</td></tr>`;
+  calendar_html += `<tr>`;
+
+  // Fill the first week of the month with the appropriate number of blanks.
+  for(week_day = 0; week_day < first_week_day; week_day++) {
+    calendar_html += `<td> </td>`;
+  }
+
+  week_day = first_week_day;
+  for(day_counter = 1; day_counter <= days_in_this_month; day_counter++) {
+    week_day %= 7;
+    if(week_day == 0)
+      calendar_html += `</tr><tr>`;
+
+    // Do something different for the current day.
+    if(day == day_counter)
+      calendar_html += `<td><span class="accent">` + day_counter + `</span></td>`;
+    else
+      calendar_html += `<td> ` + day_counter + ` </td>`;
+
+    week_day++;
+  }
+
+  calendar_html += `</tr>`;
+  calendar_html += `</table>`;
+
+  // Display the calendar.
+  return calendar_html
 }
